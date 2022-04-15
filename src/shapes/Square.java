fa3f49e35ee44c357bf4ba1213b38ca694bc33ee
desktop-o0gpqr4\antsh <ants.hernandez@gmail.com>
2022-04-14 19:19:47 -0500

    updating work

---
+++ src/shapes/Square.java
@@ -0,0 +1,46 @@
+package shapes;
+
+public class Square extends Quadrilateral {
+
+    public Square(double side){
+        super(side,side);
+    }
+
+    @Override
+    public void setLength(double length) {
+        this.length = length;
+        this.width = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
+        this.length = width;
+        this.width = width;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return length * 4;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+//    private double side;
+//
+//    public Square(double side) {
+//        super(side, side);
+//        this.side = side;
+//    }
+//
+//    public double getArea() {
+//        System.out.println("using");
+//        return side * side;
+//    }
+//
+//    public double getPerimeter() {
+//        System.out.println("using");
+//        return side * 4;
+//    }
+}
